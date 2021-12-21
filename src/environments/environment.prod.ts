75141d4d13f77115874bf36e68757541fa2a6f9b
nurjanahevs <evasitinurjanah2019@gmail.com>
2021-12-21 12:52:06 +0700

    changeurldatabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'mongodb://nurjanahevs:nurjanahves@dbeva-shard-00-00.3gs73.mongodb.net:27017,dbeva-shard-00-01.3gs73.mongodb.net:27017,dbeva-shard-00-02.3gs73.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hc0xp4-shard-0&authSource=admin&retryWrites=true&w=majority'
+  baseUrl: 'mongodb://nurjanahevs:nurjanahevs@dbeva-shard-00-00.3gs73.mongodb.net:27017,dbeva-shard-00-01.3gs73.mongodb.net:27017,dbeva-shard-00-02.3gs73.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hc0xp4-shard-0&authSource=admin&retryWrites=true&w=majority'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl : 'mongodb://nurjanahevs:nurjanahves@dbeva-shard-00-00.3gs73.mongodb.net:27017,dbeva-shard-00-01.3gs73.mongodb.net:27017,dbeva-shard-00-02.3gs73.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hc0xp4-shard-0&authSource=admin&retryWrites=true&w=majority'
+  baseUrl : 'mongodb://nurjanahevs:nurjanahevs@dbeva-shard-00-00.3gs73.mongodb.net:27017,dbeva-shard-00-01.3gs73.mongodb.net:27017,dbeva-shard-00-02.3gs73.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hc0xp4-shard-0&authSource=admin&retryWrites=true&w=majority'
   // "http://localhost:3000"
   
 };
